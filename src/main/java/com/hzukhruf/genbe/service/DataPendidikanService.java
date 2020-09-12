2110cc0c6a145a0cb95f58f6aeff0f871245fd79
hzukhruf <hudzaifahzukhruf@gmail.com>
2020-09-12 21:37:31 +0700

    delete pendidikan service

--- src/main/java/com/hzukhruf/genbe/service/DataPendidikanService.java
+++
@@ -1,9 +0,0 @@
-package com.hzukhruf.genbe.service;
-
-import java.util.List;
-
-import com.hzukhruf.genbe.model.dto.DataDto3;
-
-public interface DataPendidikanService {
-	List<DataDto3> insertdataPendidikan(Integer idPerson,List<DataDto3> dataList);
-}
